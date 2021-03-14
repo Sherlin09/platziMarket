c6bf768fa5659f569e7bcd1ab51d8c2f080dcc8b
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-03-13 22:48:35 -0600

    Usar la interface CrudRepository

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,11 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository  extends CrudRepository<Producto, Integer> {
+
+}
