c9db5d8e89505ee7b77316accba9f0014cef6c87
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-03-23 22:24:03 -0600

    5_4 Crear el repositorio de compras

--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -17,6 +17,7 @@ public class ComprasProducto {
 
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
 
@@ -58,5 +59,20 @@ public class ComprasProducto {
 
     public void setEstado(Boolean estado) {
         this.estado = estado;
+    }
+
+    public void setCompra(Compra compra) {
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 }
